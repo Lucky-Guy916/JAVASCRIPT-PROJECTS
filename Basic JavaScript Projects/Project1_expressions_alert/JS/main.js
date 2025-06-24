e0f214143b702ea3b9241da113ce9956a8d5e16e
Lucky-Guy916 <106700822+Lucky-Guy916@users.noreply.github.com>
2025-06-24 06:25:54 -0700

    Project1

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+var Sent1 = "This is the beginning of the string ";
+var Sent2 = "and this is the end of the string"; 
+//Creating the two variables and giving them a string value 
+
+ window.alert("Hello, World!") //Making a pop up alert when you open the page.
+
+ document.write(Sent1 + Sent2);//This is how you have JS display the text of 
+ // two variables concatenated together
+
+ document.write("<br>") //This is how to insert a line break
+
+document.write (3 * 3);//This is an expression
